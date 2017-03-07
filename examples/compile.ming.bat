559440691a027aaf4155af650cb4f8df34498ef2
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-03-07 20:37:17 +0100

    + msg-distributor.cpp

--- examples/compile.ming.bat
+++ examples/compile.ming.bat
@@ -1,4 +1,5 @@
-g++ -c  ../src/Socket.cpp -o Socket.o
-g++ -I../src simple-client.cpp Socket.o -lwsock32 -o simple-client.exe
-g++ -I../src echo-server.cpp   Socket.o -lwsock32 -o echo-server.exe
-g++ -I../src proxy.cpp         Socket.o -lwsock32 -o proxy.exe
+g++ -c  ../src/Socket.cpp       -o Socket.o
+g++ -I../src simple-client.cpp     Socket.o -lwsock32 -o simple-client.exe
+g++ -I../src echo-server.cpp       Socket.o -lwsock32 -o echo-server.exe
+g++ -I../src proxy.cpp             Socket.o -lwsock32 -o proxy.exe
+g++ -I../src msg-distributor.cpp   Socket.o -lwsock32 -o msg-distributor.exe
