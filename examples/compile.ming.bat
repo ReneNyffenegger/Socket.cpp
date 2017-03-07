62b9498b101909b7c53e4f65933930f619a75b04
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-03-07 20:34:05 +0100

    + proxy.cpp

--- examples/compile.ming.bat
+++ examples/compile.ming.bat
@@ -1,3 +1,4 @@
 g++ -c  ../src/Socket.cpp -o Socket.o
 g++ -I../src simple-client.cpp Socket.o -lwsock32 -o simple-client.exe
 g++ -I../src echo-server.cpp   Socket.o -lwsock32 -o echo-server.exe
+g++ -I../src proxy.cpp         Socket.o -lwsock32 -o proxy.exe
